bb63af2710c21a71401c0fd8059f3382a8ee6183
mind.spk <sikarin.spk@gmail.com>
2024-06-24 17:24:46 +0700

    ทำหน้าเมนู โดยใช้ MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Menu from './pages/Menu';
-import About from './pages/About';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
 
 const App = () => {
-    return (
-        <div>
-            
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <Menu />
+      <About />
+    </div>
+  );
 };
 
 export default App;
